b3bfa6712cf1fe94ff882865ffe8fa3bd63b56dc
slawvs <slawvs@gmail.com>
2018-11-18 13:57:35 +0100

    Delete RadioMessagesReceiver.java

--- src/RadioMessagesReceiver.java
+++
@@ -1,8 +0,0 @@
-
-public interface RadioMessagesReceiver {
-
-	public void newEmergencyCall(String call);
-	public void newInformation(String information);
-	public void newOrder(String order);
-	
-}
